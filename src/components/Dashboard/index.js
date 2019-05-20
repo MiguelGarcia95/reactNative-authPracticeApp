b80d4d65a24b4637e89923b3275d7154878aa3fb
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-20 17:13:04 -0500

    disable header

--- src/components/Dashboard/index.js
+++ src/components/Dashboard/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react'
 import { Text, View } from 'react-native'
 
 class Dashboard extends Component {
+  static navigationOptions = {
+    header: null
+  }
+  
   render() {
     return (
       <View>
