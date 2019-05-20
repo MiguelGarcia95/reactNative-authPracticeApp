01bb8d485340ef8507a5f663da7ba8792a4c4dc1
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-20 12:39:20 -0500

    create home page

--- App.js
+++ App.js
@@ -1,54 +1,10 @@
 import React, {Component} from 'react';
-import {StyleSheet, Text, View, Button} from 'react-native';
 import { createStackNavigator, createAppContainer } from "react-navigation";
-
-class App extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Home Screen!</Text>
-        <Button
-          title="Go to Test"
-          onPress={() => this.props.navigation.navigate('Test')}
-        />
-      </View>
-    );
-  }
-}
-
-class Test extends Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>Test Screen!</Text>
-      </View>
-    );
-  }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
-  },
-});
+import Home from './src/components/Home';
 
 const AppNavigator = createStackNavigator(
   {
-    Home: App,
-    Test: Test
+    Home: Home,
   },
   {
     initialRouteName: "Home"
