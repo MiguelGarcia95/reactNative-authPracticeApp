2ef9e2231152f178e4bce8cff506979f035e449c
MiguelGarcia95 <mgarcia95951@gmail.com>
2019-05-20 14:27:52 -0500

    create login function

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -3,19 +3,23 @@ import { Text, View, TextInput, Button } from 'react-native'
 import styles from './styles';
 
 class Home extends Component {
-  checkLogin() {
+  state = {username: '', password: ''}
 
+  checkLogin = () => {
+    const {username, password} = this.state;
+    console.log(username);
+    console.log(password);
   }
-  
+
   render() {
     const {heading, input, button, parent} = styles;
     return (
       <View style={parent}>
         <Text style={heading}>Login into the Application.</Text>
-        <TextInput style={input} />
-        <TextInput style={input} secureTextEntry={true} />
+        <TextInput style={input}  placeholder='Username' onChangeText={text => this.setState({username: text})} />
+        <TextInput style={input} secureTextEntry={true} placeholder='Password' />
 
-        <Button style={button} title={'Login'} onPress={_ => this.checkLogin} />
+        <Button style={button} title={'Login'} onPress={this.checkLogin} />
       </View>
     )
   }
